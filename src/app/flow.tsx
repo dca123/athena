54cfb34b8941b52acba6e5aec744c61302a480b9
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-11-01 20:01:40 +0800

    init

---
+++ src/app/flow.tsx
@@ -0,0 +1,67 @@
+'use client';
+import { useCallback, useState } from 'react';
+import ReactFlow, {
+  Background,
+  Controls,
+  Node,
+  Edge,
+  applyEdgeChanges,
+  applyNodeChanges,
+  NodeChange,
+  EdgeChange,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
+
+const initialNodes: Array<Node> = [
+  {
+    id: '1',
+    position: { x: 0, y: 0 },
+    data: { label: 'Hello' },
+    type: 'input',
+  },
+  {
+    id: '2',
+    position: { x: 100, y: 100 },
+    data: { label: 'World' },
+  },
+];
+const initialEdges: Array<Edge> = [
+  {
+    id: '1-2',
+    source: '1',
+    target: '2',
+    animated: true,
+    label: 'to the',
+    type: 'smoothstep',
+  },
+];
+
+export function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nds) => applyNodeChanges(changes, nds)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [],
+  );
+
+  return (
+    <div className="w-full h-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+}
